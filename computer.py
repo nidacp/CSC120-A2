4186c379466fc963e8efffac60e217a767e8f0c8
nidacp <145049835+nidacp@users.noreply.github.com>
2023-09-29 19:53:21 -0400

    Update computer.py

--- computer.py
+++ computer.py
@@ -1,10 +1,47 @@
+from typing import Optional
 class Computer:
+    # /usr/local/bin/python3 "/Users/nida/smithassignments/CSC120-A2/computer.py"
 
     # What attributes will it need?
 
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
+
+
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, description: str,
+                    processor_type: str,
+                    hard_drive_capacity: int,
+                    memory: int,
+                    operating_system: str,
+                    year_made: int,
+                    price: int):
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price = price
 
     # What methods will you need?
+
+    # refurbish
+    def refurbish(self, new_os):
+        #changes the OS and changes price based on how old the computer is
+
+        self.operating_system = new_os
+        if int(self.year_made) < 2000:
+            self.price = 0 # too old to sell, donation only
+        elif int(self.year_made) < 2012:
+            self.price = 250 # heavily-discounted price on machines 10+ years old
+        elif int(self.year_made) < 2018:
+            self.price = 550 # discounted price on machines 4-to-10 year old machines
+        else:
+            self.price = 1000 # recent stuff
