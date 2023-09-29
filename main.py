cec54bfe542387f50f33663a239025813b010b29
nidacp <145049835+nidacp@users.noreply.github.com>
2023-09-29 19:54:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # Import a few useful containers from the typing module
 from typing import Dict, Union
 
+
 # Import the functions we wrote in procedural_resale_shop.py
-from procedural_resale_shop import buy, update_price, sell, print_inventory, refurbish
+from computer import Computer
+from oo_resale_shop import ResaleShop
 
 """ This helper function takes in a bunch of information about a computer,
     and packages it up into a python dictionary to make it easier to store
@@ -11,31 +13,18 @@ from procedural_resale_shop import buy, update_price, sell, print_inventory, ref
     explicit data types (str, int, etc.) listed in a python function. We're 
     going to go the extra step, because when we get to Java it'll be required!
 """
-def create_computer(description: str,
-                    processor_type: str,
-                    hard_drive_capacity: int,
-                    memory: int,
-                    operating_system: str,
-                    year_made: int,
-                    price: int):
-    return {'description': description,
-            'processor_type': processor_type,
-            'hard_drive_capacity': hard_drive_capacity,
-            'memory': memory,
-            'operating_system': operating_system,
-            'year_made': year_made,
-            'price': price
-    }
 
 def main():
     
     # First, let's make a computer
-    computer = create_computer(
+    computer = Computer(
         "Mac Pro (Late 2013)",
         "3.5 GHc 6-Core Intel Xeon E5",
         1024, 64,
         "macOS Big Sur", 2013, 1500
     )
+    inv = []
+    resale = ResaleShop(inv)
 
     # Print a little banner
     print("-" * 21)
@@ -43,35 +32,36 @@ def main():
     print("-" * 21)
 
     # Add it to the resale store's inventory
-    print("Buying", computer["description"])
+    print("Buying", computer.description)
     print("Adding to inventory...")
-    computer_id = buy(computer)
+    resale.buy(computer)
     print("Done.\n")
 
     # Make sure it worked by checking inventory
     print("Checking inventory...")
-    print_inventory()
+    resale.print_inventory()
     print("Done.\n")
 
     # Now, let's refurbish it
     new_OS = "MacOS Monterey"
-    print("Refurbishing Item ID:", computer_id, ", updating OS to", new_OS)
+    print("Refurbishing Item ID: 1", ", updating OS to", new_OS)
     print("Updating inventory...")
-    refurbish(computer_id, new_OS)
+    computer.refurbish(new_OS)
     print("Done.\n")
 
     # Make sure it worked by checking inventory
     print("Checking inventory...")
-    print_inventory()
+    resale.print_inventory()
     print("Done.\n")
     
     # Now, let's sell it!
-    print("Selling Item ID:", computer_id)
-    sell(computer_id)
+    print("Selling Item ID: 1")
+    resale.sell(computer)
+    print("Item sold!")
     
     # Make sure it worked by checking inventory
     print("Checking inventory...")
-    print_inventory()
+    resale.print_inventory()
     print("Done.\n")
 
 # Calls the main() function when this file is run
